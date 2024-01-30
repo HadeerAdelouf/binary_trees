04075b1bf23068a461de7017d2f21e1981b8edc7
HadeerAdelouf <hadiradel999@gmail.com>
2024-01-31 00:02:43 +0200

    12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_leaves - counts the leaves in a binary tree
-* @tree: tree to count the leaves from
-* Return: number of leaves or 0 if tree is NULL
-*/
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: tree to count the leaves from
+ * Return: number of leaves or 0 if tree is NULL
+ */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (!tree)
@@ -13,8 +13,5 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (!tree->left && !tree->right)
 		return (1);
 
-	size_t left_leaves = binary_tree_leaves(tree->left);
-	size_t right_leaves = binary_tree_leaves(tree->right);
-
-	return (left_leaves + right_leaves);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
